15daeeb19ea23e324a1505062470953dc518d8f7
Gabriel Machado <gabrielchiles.engenharia@gmail.com>
2026-05-19 18:57:34 -0300

    chore: baseline inicial com estrutura frontend e backend

---
+++ backend/mylibrary-backend/src/main/java/com/senai/mylibrary_backend/MylibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.senai.mylibrary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylibraryBackendApplication.class, args);
+	}
+
+}
---
+++ backend/mylibrary-backend/src/test/java/com/senai/mylibrary_backend/MylibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.mylibrary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
